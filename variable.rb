41914382e11fd3f3bb224d92a43a22baeb7bce34
Matt Deming <vballmatt44@gmail.com>
2022-02-07 21:03:23 -0700

    Anotate some code

--- variable.rb
+++ variable.rb
@@ -1,11 +1,14 @@
 name = "Matt"
+# Variable 'from' used to apply string 'Somewhere in California'
 from = "Somewhere in California."
 favorite_animal = "Snow leopard and dragons..."
 
+# Variable 'age' shows as integer '34'
 age = 34 # years
 weight = 175 # lbs
 height = 74 # in inches
 
+# Variable 'account_balance' applies Float number as '15.75'
 account_balance = 15.75 # dollars
 favorite_number = 44.88
 pi = 3.141 # this is all i remember for pi
