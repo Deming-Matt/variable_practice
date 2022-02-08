234e92845a0e6a99936a9e6800652af9fb2733fa
Matt Deming <vballmatt44@gmail.com>
2022-02-07 20:55:05 -0700

    Share float numbers

--- variable.rb
+++ variable.rb
@@ -5,3 +5,7 @@ favorite_animal = "Snow leopard and dragons..."
 age = 34 # years
 weight = 175 # lbs
 height = 74 # in inches
+
+account_balance = 15.75 # dollars
+favorite_number = 44.88
+pi = 3.141 # this is all i remember for pi
